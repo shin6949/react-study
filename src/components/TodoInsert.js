92d0257dfdac0e6a1cda2257665d62cc812c5086
shin6949 <cocoblue@kakao.com>
2022-05-10 16:12:00 +0900

    Finish Chapter 10

--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -1,10 +1,28 @@
+import { useCallback, useState } from 'react';
 import { MdAdd } from 'react-icons/md';
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
+  // 정말 주의하자. {}이 아닌 []이다.
+  const [value, setValue] = useState('');
+
+  const onChange = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+
+  // onClick로 이벤트를 정의할 수는 있지만, Enter 키를 통해 입력할 수 없게 된다.
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      onInsert(value);
+      setValue('');
+    },
+    [onInsert, value],
+  );
+
   return (
-    <form className="TodoInsert">
-      <input placeholder="할 일을 입력하세요." />
+    <form className="TodoInsert" onSubmit={onSubmit}>
+      <input placeholder="할 일을 입력하세요." value={value} onChange={onChange} />
       <button type="submit">
         <MdAdd />
       </button>
